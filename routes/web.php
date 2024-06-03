8b006df71999e2f6cab9fa424552060ca3d51bb7
Bayu Kevin Farindra <bayukevinfarindra23@gmail.com>
2024-06-04 04:45:15 +0700

    bayukevin-add layout

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,15 @@ use App\Http\Controllers\BarangController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('barang.dashboard');
+})->name('dashboard');
+
+Route::get('/barang-masuk', function () {
+    return view('barang.barang-masuk');
+})->name('barangmasuk');
+
+Route::get('/laporan-pemasukan', function () {
+    return view('barang.laporan-pemasukan');
+})->name('laporanpemasukan');
