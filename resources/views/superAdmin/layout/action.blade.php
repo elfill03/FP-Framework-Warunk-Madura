95db71ca0df72065048176733e694996ea725366
Hugo Rayhan Firmansyah <125570549+HgRayhan08@users.noreply.github.com>
2024-06-11 18:28:54 +0700

    Delete resources/views/superAdmin/layout/action.blade.php

--- resources/views/superAdmin/layout/action.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <a href="/super/edit-admin" class="btn btn-warning"><i class="bx bx-edit text-white"></i></a>
-    <a class="btn btn-danger"><i class="bx bx-trash text-white"></i></a>
-</div>
